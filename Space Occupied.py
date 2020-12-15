8caad8507ef24f41c16edbf1a534f0462c0093a7
KaiTech-git <kajetan.chodorowski@gmail.com>
2020-12-16 00:54:40 +0100

    Space_fixing

--- Space Occupied.py
+++ Space Occupied.py
@@ -1,17 +1,19 @@
 
-def size_r(path, size =0):
+def size_r(path):
+    size = 0
     arr = os.listdir(path)
     for x in range(len(arr)):
         if os.path.isfile (path + "/" + arr[x]):
            size = size + os.stat(path + "/" + arr[x]).st_size
         else:
             path_x = path + "/" + arr[x]
-            size_r(path_x, size)
+            size += size_r(path_x)
+            return size
     return size
 
 
 import os
 full_path = input("give path:")
 
-size =size_r(full_path, 0)
+size =size_r(full_path)
 print(size)
